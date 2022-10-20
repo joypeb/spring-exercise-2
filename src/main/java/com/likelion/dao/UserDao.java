2be191936d9445d6c632cbf8f838470d374dc388
박은빈 <joypeb@naver.com>
2022-10-20 15:25:58 +0900

    UserDao 에러 핸들링

--- src/main/java/com/likelion/dao/UserDao.java
+++ src/main/java/com/likelion/dao/UserDao.java
@@ -17,95 +17,159 @@ public class UserDao {
     }
 
     public void add(User user) {
+        Connection c = null;
+        PreparedStatement ps =  null;
         try {
             // DB접속 (ex sql workbeanch실행)
-            Connection c = connectionMaker.makeConnection();
+            c = connectionMaker.makeConnection();
 
             // Query문 작성
-            PreparedStatement pstmt = c.prepareStatement("INSERT INTO likelionDB.users(id, name, password) VALUES(?,?,?);");
-            pstmt.setString(1, user.getId());
-            pstmt.setString(2, user.getName());
-            pstmt.setString(3, user.getPassword());
+            ps = c.prepareStatement("INSERT INTO likelionDB.users(id, name, password) VALUES(?,?,?);");
+            ps.setString(1, user.getId());
+            ps.setString(2, user.getName());
+            ps.setString(3, user.getPassword());
 
             // Query문 실행
-            pstmt.executeUpdate();
-
-            pstmt.close();
-            c.close();
+            ps.executeUpdate();
 
         } catch (SQLException e) {
             throw new RuntimeException(e);
+        } finally {
+            if(ps != null) {
+                try {
+                    ps.close();
+                } catch (SQLException e) {
+                }
+            }
+            if(c != null) {
+                try {
+                    c.close();
+                } catch (SQLException e) {
+                }
+            }
         }
     }
 
     public void deleteAll() {
+        Connection c = null;
+        PreparedStatement ps = null;
         try {
-            Connection c = connectionMaker.makeConnection();
+            c = connectionMaker.makeConnection();
 
-            PreparedStatement ps = c.prepareStatement(
+            ps = c.prepareStatement(
                     "DELETE FROM likelionDB.users"
             );
 
             ps.executeUpdate();
 
-            ps.close();
-            c.close();
         } catch (SQLException e) {
             throw new RuntimeException(e);
+        } finally {
+            if(ps != null) {
+                try {
+                    ps.close();
+                } catch (SQLException e) {
+                }
+            }
+            if(c != null) {
+                try {
+                    c.close();
+                } catch (SQLException e) {
+                }
+            }
         }
     }
 
     public int getCount() {
+        Connection c = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
         try {
-            Connection c = connectionMaker.makeConnection();
+            c = connectionMaker.makeConnection();
 
-            PreparedStatement ps = c.prepareStatement(
+            ps = c.prepareStatement(
                     "SELECT COUNT(*) FROM likelionDB.users"
             );
 
-            ResultSet rs = ps.executeQuery();
+            rs = ps.executeQuery();
 
             rs.next();
             int count = rs.getInt(1);
 
-            rs.close();
-            ps.close();
-            c.close();
-
             return count;
         } catch (SQLException e) {
             throw new RuntimeException(e);
+        } finally {
+            if(rs != null) {
+                try {
+                    rs.close();
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            if(ps != null) {
+                try {
+                    ps.close();
+                } catch (SQLException e) {
+                }
+            }
+            if(c != null) {
+                try {
+                    c.close();
+                } catch (SQLException e) {
+                }
+            }
         }
     }
 
     public User findById(String id) {
-        try {
-            Connection c = connectionMaker.makeConnection();
-            // Query문 작성
-            PreparedStatement pstmt = c.prepareStatement("SELECT * FROM likelionDB.users WHERE id = ?");
-            pstmt.setString(1, id);
+        Connection c = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        {
+            try {
+                c = connectionMaker.makeConnection();
+                // Query문 작성
+                ps = c.prepareStatement("SELECT * FROM likelionDB.users WHERE id = ?");
+                ps.setString(1, id);
 
-            // Query문 실행
-            ResultSet rs = pstmt.executeQuery();
+                // Query문 실행
+                rs = ps.executeQuery();
 
-            User user = null;
+                User user = null;
 
-            if (rs.next()) {
-                user = new User(rs.getString("id"), rs.getString("name"),
-                        rs.getString("password"));
+                if (rs.next()) {
+                    user = new User(rs.getString("id"), rs.getString("name"),
+                            rs.getString("password"));
+                }
+
+                //데이터가 없을시 에러처리
+                if (user == null) throw new EmptyResultDataAccessException(1);
+                return user;
+
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            } finally {
+                if (rs != null) {
+                    try {
+                        rs.close();
+                    } catch (SQLException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+                if (ps != null) {
+                    try {
+                        ps.close();
+                    } catch (SQLException e) {
+                    }
+                }
+                if (c != null) {
+                    try {
+                        c.close();
+                    } catch (SQLException e) {
+                    }
+                }
             }
-
-            rs.close();
-            pstmt.close();
-            c.close();
-
-            //데이터가 없을시 에러처리
-            if(user == null) throw new EmptyResultDataAccessException(1);
-
-            return user;
-
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
         }
     }
 }

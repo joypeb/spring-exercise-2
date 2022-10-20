c29b4202c99566829a500a0a265408b236dbd42b
박은빈 <joypeb@naver.com>
2022-10-20 13:29:21 +0900

    testCount추가

--- src/test/java/com/likelion/dao/UserDaoTest.java
+++ src/test/java/com/likelion/dao/UserDaoTest.java
@@ -1,7 +1,6 @@
 package com.likelion.dao;
 
 import com.likelion.domain.User;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,6 +31,27 @@ class UserDaoTest {
         assertEquals(id,user.getId());
         assertEquals("test1",user.getName());
         assertEquals(1,userDao.getCount());
+    }
 
+
+    @Test
+    public void testCount() {
+        UserDao userDao = context.getBean("awsUserDao", UserDao.class);
+
+        User u1 = new User("1","박은빈","pw1");
+        User u2 = new User("2","박은빈2","pw2");
+        User u3 = new User("3","ParkEunBin","pw3");
+
+        userDao.deleteAll();
+        assertEquals(0,userDao.getCount());
+
+        userDao.add(u1);
+        assertEquals(1,userDao.getCount());
+
+        userDao.add(u2);
+        assertEquals(2,userDao.getCount());
+
+        userDao.add(u3);
+        assertEquals(3,userDao.getCount());
     }
 }
